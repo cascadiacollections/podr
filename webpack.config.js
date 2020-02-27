8cf8947ce8d022ff0a075de1ff9d009db33b40ed
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2020-02-26 22:32:23 -0800

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
+'use strict';
+
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: './lib/src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js'
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development | SModr',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   }
 };
