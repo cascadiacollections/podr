802874493b7576055ead787dd659a9d0930c6379
Kevin T. Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2024-10-19 23:57:12 +0000

    Add comments

--- src/ui/Search.tsx
+++ src/ui/Search.tsx
@@ -1,11 +1,29 @@
 import { h, JSX, FunctionComponent } from 'preact';
 import { useCallback } from 'preact/hooks';
 
+/**
+ * Props for the Search component.
+ */
 interface ISearchProps {
+  /**
+   * Callback to be called when the user submits the search query.
+   *
+   * @param query the search query
+   * @returns void
+   */
   onSearch: (query: string) => void;
 }
 
+/**
+ * Search component.
+ *
+ * @param onSearch
+ * @returns h.JSX.HTMLAttributes<HTMLInputElement>
+ */
 export const Search: FunctionComponent<ISearchProps> = ({ onSearch }) => {
+  /**
+   * Handle the keydown event on the search input.
+   */
   const onKeyDown = useCallback((event: JSX.TargetedKeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       onSearch(event.currentTarget.value.trim());
